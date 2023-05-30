ca118145dbd9444ac0feeb28e12b71da85aff124
debscalo <debora.caloiero@gmail.com>
2023-05-30 09:27:59 +0200

    lastAdd

---
+++ Clinica/src/main/java/Clinica.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Clinica {
+
+    private Map<String, Specialista> oculistaList = new HashMap<>();
+
+
+    public Clinica(){
+
+
+    }
+
+    public void prenota(Specialista specialista, Paziente paziente) {
+
+    }
+}
---
+++ Clinica/src/main/java/Specialista.java
@@ -0,0 +1,1 @@
+public enum Specialista {OCULISTA, PEDIATRA}
